c157d2289252b6b3e184b59c9ae45d9c882f2e54
Ju Liu <ju@noredink.com>
2019-12-09 22:52:28 -0800

    Add back fortune and set some LC env variables

--- home.nix
+++ home.nix
@@ -32,6 +32,7 @@ in {
     pkgs.bashInteractive
     pkgs.coreutils
     pkgs.direnv
+    pkgs.fortune
     pkgs.git
     pkgs.htop
     pkgs.nixfmt
@@ -74,6 +75,10 @@ in {
   programs.bash = {
     enable = true;
     profileExtra = ''
+      export LANG=en_US.UTF-8
+      export LC_CTYPE=en_US.UTF-8
+      export LC_ALL=en_US.UTF-8
+
       . "$HOME/.nix-profile/etc/profile.d/nix.sh"
       . "$HOME/.nix-profile/etc/profile.d/bash_completion.sh"
       . "$HOME/.nix-profile/etc/bash_completion.d/git-prompt.sh"
