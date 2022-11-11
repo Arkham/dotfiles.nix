dbbecbd2917330b6cf470bf709680b3f774b4f76
Ju Liu <liuju86@gmail.com>
2022-11-11 10:28:46 +0000

    Stop ignoring tags

--- git.nix
+++ git.nix
@@ -3,7 +3,7 @@
     enable = true;
     userName = "Ju Liu";
     userEmail = "liuju86@gmail.com";
-    ignores = [ "*~" ".DS_Store" ".direnv" ".env" "tags" ".rgignore" ];
+    ignores = [ "*~" ".DS_Store" ".direnv" ".env" ".rgignore" ];
     extraConfig = { pull = { ff = "only"; }; };
     lfs = { enable = true; };
     delta = { enable = true; };
