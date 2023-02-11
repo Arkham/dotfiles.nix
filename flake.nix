5914a5d728be35413a1701d8da8a228f809f54ae
Ju Liu <liuju86@gmail.com>
2023-02-11 18:02:54 +0000

    Add nix-index flake so that comma works

--- flake.nix
+++ flake.nix
@@ -7,19 +7,23 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: let
-    system = "x86_64-darwin";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    defaultPackage.${system} = home-manager.defaultPackage.${system};
+  outputs = { nixpkgs, home-manager, nix-index-database, ... }:
+    let
+      system = "x86_64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      defaultPackage.${system} = home-manager.defaultPackage.${system};
 
-    homeConfigurations.arkham =
-      home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.arkham = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        modules = [ ./home.nix ];
+        modules = [ ./home.nix nix-index-database.hmModules.nix-index ];
       };
     };
 }
