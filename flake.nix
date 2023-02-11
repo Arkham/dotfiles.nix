a28e580693531820b0b47aa445e168ceb5d33e27
Ju Liu <liuju86@gmail.com>
2023-02-11 18:57:59 +0000

    Support both intel and arm macs

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nix-index-database = {
       url = "github:Mic92/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,15 +17,19 @@
 
   outputs = { nixpkgs, home-manager, nix-index-database, ... }:
     let
-      system = "x86_64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      defaultPackage.${system} = home-manager.defaultPackage.${system};
-
-      homeConfigurations.arkham = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
+      config = system: {
+        pkgs = nixpkgs.legacyPackages.${system};
         modules = [ ./home.nix nix-index-database.hmModules.nix-index ];
       };
+    in {
+      defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+      defaultPackage.aarch64-darwin =
+        home-manager.defaultPackage.aarch64-darwin;
+
+      homeConfigurations."arkham@mine" =
+        home-manager.lib.homeManagerConfiguration (config "x86_64-darwin");
+
+      homeConfigurations."arkham@metal" =
+        home-manager.lib.homeManagerConfiguration (config "aarch64-darwin");
     };
 }
