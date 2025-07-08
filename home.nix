db5088fb3640efc5c8b02ec545d06fc954548063
Ju Liu <liuju86@gmail.com>
2025-07-08 16:19:01 +0100

    Update to 25.05

--- home.nix
+++ home.nix
@@ -2,7 +2,7 @@
   home.username = "arkham";
   home.homeDirectory =
     "/${if pkgs.stdenv.isDarwin then "Users" else "home"}/arkham";
-  home.stateVersion = "24.05";
+  home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 
   imports = [ ./packages.nix ./vim.nix ./git.nix ./rest.nix ];
